8d53f3bd7845c05a8fc0816d38f5ef3b428c645e
Liunx <liunx163@163.com>
2021-02-01 18:09:10 +0800

    add hydrogen export support.

--- drummer.py
+++ drummer.py
@@ -1,6 +1,9 @@
 import time
 import pygame
+import xmltodict
 from fractions import Fraction
+from parameters import Percussion
+from collections import OrderedDict
 
 
 instruments = [
@@ -163,6 +166,73 @@ class Drummer:
         return _beats
 
 
+class Hydrogen:
+    pattern_size = 192
+    template = 'data/default.h2song'
+    pattern_dict = OrderedDict([('name', 'pattern 1-1'),
+                                ('category', None),
+                                ('size', '192'),
+                                ('info', None),
+                                ('noteList', None)])
+    note_dict = OrderedDict([('position', '0'),
+                             ('leadlag', '0'),
+                             ('velocity', '0.8'),
+                             ('pan_L', '0.5'),
+                             ('pan_R', '0.5'),
+                             ('pitch', '0'),
+                             ('probability', '1'),
+                             ('key', 'C0'),
+                             ('length', '-1'),
+                             ('instrument', '0'),
+                             ('note_off', 'false')])
+
+    def __init__(self):
+        with open(self.template) as f:
+            self.default = xmltodict.parse(f.read(), process_namespaces=True)
+            self.default['song']['patternList']['pattern'] = []
+
+    def load_instruments(self, xml):
+        self.midi_map = {}
+        with open(xml) as f:
+            drumkit = xmltodict.parse(f.read())
+        instruments = drumkit['drumkit_info']['instrumentList']['instrument']
+        for i in instruments:
+            i['drumkit'] = drumkit['drumkit_info']['name']
+            self.midi_map[int(i['midiOutNote'])] = i
+        self.default['song']['instrumentList'] = {'instrument': instruments}
+
+    def save(self, xml):
+        with open(xml, 'w') as f:
+            f.write(xmltodict.unparse(self.default, pretty=True))
+
+    def add_note(self, instrument, offset):
+        ndict = self.note_dict.copy()
+        ndict['position'] = offset
+        ndict['instrument'] = instrument['id']
+        return ndict
+
+    def add_notes(self, instrument, pattern):
+        notes = []
+        plen = len(pattern)
+        if self.pattern_size % plen > 0:
+            raise ValueError('Pattern length is fault!')
+        unit = self.pattern_size // plen
+        for i in range(plen):
+            if pattern[i] > 0:
+                notes.append(self.add_note(instrument, i * unit))
+        return notes
+
+    def add_pattern(self, name, pattern):
+        pdict = self.pattern_dict.copy()
+        pdict['name'] = name
+        notes = []
+        for k, v in pattern.items():
+            if k in self.midi_map:
+                notes += self.add_notes(self.midi_map[k], v)
+        pdict['noteList'] = {'note': notes}
+        self.default['song']['patternList']['pattern'].append(pdict)
+
+
 def demo01():
     song = [rock_prime[0], rock_vice[0]] * 2
     song += [rock_prime[1], rock_vice[1]] * 2
@@ -172,5 +242,22 @@ def demo01():
     drum.play_song(4, 110, song)
 
 
+def demo02():
+    rock_prime = [
+        {
+            Percussion.BassDrum1: [1, 0, 0, 0, 1, 1, 0, 0],
+            Percussion.ElectricSnare: [0, 1, 0, 1],
+            Percussion.ClosedHiHat: [1] * 8,
+        },
+    ]
+    s = '/home/leiliu/Work/sources/hydrogen/data/drumkits/TR808EmulationKit/drumkit.xml'
+    h2 = Hydrogen()
+    h2.load_instruments(s)
+    for p in rock_prime:
+        h2.add_pattern('rock01', p)
+
+    h2.save('xxx.h2song')
+
+
 if __name__ == '__main__':
-    demo01()
+    demo02()
